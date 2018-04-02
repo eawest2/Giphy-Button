391d9afb9f19d2535705958ca74bb451be2ee278
unknown <eawest2@gmail.com>
2018-04-02 16:10:39 -0400

    Completed

--- code.js
+++ code.js
@@ -3,17 +3,12 @@
 //Variable Storage:
 
     //Array of all buttons on the page
-    var buttonArray = ["elves","dwarves","orcs","hobbits","minotaur","drow","merfolk", "humans", "dragonborn", "tiefling"]
+    var buttonArray = ["elves","dwarves","orcs","hobbits","minotaur","drow","mermaid", "humans", "dragonborn", "undead"]
 
     //Empty variable for URL composition
-
     var searchTerm = 0;
 
-    // Query URL composition
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+searchTerm;
-
     // Empty variable to store button clicks
-
     var buttonClick = 0;
 
 //basic functions
@@ -23,7 +18,7 @@
         $("#button-holder").text(" ")
         for (var i = 0; i < buttonArray.length; i++){
             var activeButton = buttonArray[i];
-            var activeButtonClicker = "<button " + ' data-button="' + activeButton + '" class = "gif-button">' + activeButton + "</button> ";
+            var activeButtonClicker = "<button " + ' data-button="' + activeButton + '" class = "gif-button btn btn-secondary">' + activeButton + "</button> ";
 
         $("#button-holder").append(activeButtonClicker)
 
@@ -32,17 +27,23 @@
     };
 
     //Append new search term to buttonArray
-    function addButton () {
+    function addButton() {
         var subject = $("#text-input").val().trim().toLowerCase();
         buttonArray.push(subject);
         writeButtons();
+        clearText();
     };
 
-//on click for giphButton class
-$(document).on("click", ".gif-button", function() {
-    //retrieve gifs and write to HTML
+    //clear out text from the new button search
+    function clearText() {
+        document.getElementById("text-input").value = "";
+    };
+
+    //on click listener for giphButton class
+    $(document).on("click", ".gif-button", function() {
+        //retrieve gifs and write to HTML
             // Grabbing and storing the data-button property value from the button
-        var atribution = $(this).attr("data-button");
+            var atribution = $(this).attr("data-button");
         
             // Constructing a queryURL using the button name
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -53,10 +54,10 @@ $(document).on("click", ".gif-button", function() {
                 url: queryURL,
                 method: "GET"
             })
-                // After data comes back from the request
+                //Actual process
                 .then(function(response) {
 
-                // storing the data from the AJAX request in the results variable
+                // storing the AJAX as a variable for less typing.
                 var results = response.data;
                 $("#gif-display").text(" ");
         
@@ -64,8 +65,16 @@ $(document).on("click", ".gif-button", function() {
                 for (var i = 0; i < results.length; i++) {
                     var imgUrlStill = results[i].images.fixed_height_still.url;
                     var imgUrlMove = results[i].images.fixed_height.url;
+                    var buildRating = results[i].rating;
+                    var buildSource = results[i].source;
+                    var rating = '<p class = "text-success"> Gif Rating: ' + buildRating + '</p>';
                     var construct = '<p><img class ="click-gif" data-status= "still" data-still= "'+imgUrlStill + '" ' + 'data-move = "' + imgUrlMove + '" src= "'+ imgUrlStill + '" ></p>';
+                    var source = '<a href = "'+ buildSource + '" target = "_blank" class = "text-success">' +buildSource + "<hr>"
                     $("#gif-display").append(construct);
+                    $("#gif-display").append(rating);
+                    $("#gif-display").append(source);
+
+
         
                     
                 }
@@ -73,29 +82,23 @@ $(document).on("click", ".gif-button", function() {
     
 });
 
-// Pause and unpause gifs
-
-$(document).on("click", ".click-gif", function() {
-    //select element
-    var state = $(this).attr("data-status");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-move"));
-        $(this).attr("data-status", "move");
-    } 
-    // Else set src to data-still 
-    else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-status", "still");
-    }
+    //Pause and unpause images listener
+    $(document).on("click", ".click-gif", function() {
+        //select element and check status
+        var state = $(this).attr("data-status");
+        // if the status is still, switch it to move
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-move"));
+            $(this).attr("data-status", "move");
+        } 
+        // if it's anything else, set the image to still.
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-status", "still");
+        }
     });
 
-
-
-
-    //on click for seatch button
+    //on click listener for submit button
 
     $("#submit-button").on("click", function(){
         var subjectLine = $("#text-input").val().trim().toLowerCase();
